4c92c28cd0c1cd0cbca4dbba38e3db56fb1c5819
Prakash Subedi <prakash@theironyard.com>
2017-03-29 14:11:19 -0400

    Consuming REST init

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,39 @@
+package hello;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+/**
+ * Created by psubedi2020 on 3/29/17.
+ */
+@SpringBootApplication
+public class Application {
+
+   private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String args[]) {
+        SpringApplication.run(Application.class);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Quote quote = restTemplate.getForObject(
+                    "http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+            logger.info(quote.toString());
+        };
+    }
+}
+
+
